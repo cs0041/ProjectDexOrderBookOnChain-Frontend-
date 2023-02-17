8894a0cecedee60c23f35572514b058be7ba6903
cs0041 <pamzalove20@hotmail.com>
2023-02-18 00:52:32 +0700

    ts-ignore

--- components/TradingviewGraph.tsx
+++ components/TradingviewGraph.tsx
@@ -22,73 +22,74 @@ export const ChartComponent = () => {
 
   const { tradingViewList } = useContext(ContractContext)
 
-
-
-
   useEffect(() => {
     // console.log('tradingViewList', tradingViewList)
     // if (tradingViewList.length >0) setDataTradingView(convertToOHLC(tradingViewList))
     const handleResize = () => {
-       chart.current.applyOptions({
-         width: chartContainerRef.current.clientWidth,
-         height: chartContainerRef.current.clientHeight,
-       })
+      // @ts-ignore
+      chart.current.applyOptions({
+        // @ts-ignore
+        width: chartContainerRef.current.clientWidth,
+        // @ts-ignore
+        height: chartContainerRef.current.clientHeight,
+      })
     }
-    chart.current = createChart(chartContainerRef.current,{
-                handleScale: {
-                    axisPressedMouseMove: true
-                },
-                layout: {
-                    background: {
-                      color: '#101018'
-                    },
-                    textColor: 'rgba(255, 255, 255, 0.9)',
-                },
-                grid: {
-                    vertLines: {
-                        color: '#334158',
-                    },
-                    horzLines: {
-                        color: '#334158',
-                    },
-                },
-                crosshair: {
-                    mode: CrosshairMode.Normal,
-
-                },
-                priceScale: {
-                    borderColor: '#485c7b',
-                },
-                width: chartContainerRef.current.clientWidth,
-                height: 654,
-                timeScale: {
-                    timeVisible: true,
-                    secondVisible: false,
-                    borderColor: '#485c7b',
-                }
-            } )
+    // @ts-ignore
+    chart.current = createChart(chartContainerRef.current, {
+      handleScale: {
+        axisPressedMouseMove: true,
+      },
+      layout: {
+        background: {
+          color: '#101018',
+        },
+        textColor: 'rgba(255, 255, 255, 0.9)',
+      },
+      grid: {
+        vertLines: {
+          color: '#334158',
+        },
+        horzLines: {
+          color: '#334158',
+        },
+      },
+      crosshair: {
+        mode: CrosshairMode.Normal,
+      },
+      priceScale: {
+        borderColor: '#485c7b',
+      },
+      // @ts-ignore
+      width: chartContainerRef.current.clientWidth,
+      height: 654,
+      timeScale: {
+        timeVisible: true,
+        secondVisible: false,
+        borderColor: '#485c7b',
+      },
+    })
+    // @ts-ignore
     chart.current.timeScale().fitContent()
-
+    // @ts-ignore
     candleSeriesRef.current = chart.current.addCandlestickSeries()
     // candleSeries.setData(tradingViewList)
- 
 
     window.addEventListener('resize', handleResize)
-    
+
     return () => {
       window.removeEventListener('resize', handleResize)
+      // @ts-ignore
       chart.current.remove()
     }
   }, [])
 
-
-    useEffect(() => {
-      if(chart.current)
+  useEffect(() => {
+    if (chart.current)
+      // @ts-ignore
       candleSeriesRef.current.setData(tradingViewList)
-    }, [tradingViewList]);
-  
-  
+  }, [tradingViewList])
 
+  // @ts-ignore
   return <div ref={chartContainerRef} />
 }
 
