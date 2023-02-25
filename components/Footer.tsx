68b358541e836845838183052ccce4270c33eacd
cs0041 <pamzalove20@hotmail.com>
2023-02-25 15:48:35 +0700

    social media

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
 import { WifiIcon } from '@heroicons/react/24/outline'
 import { ChartBarIcon } from '@heroicons/react/24/solid'
+import twitter from '../public/images/images/twitter.svg'
+import instagram from '../public/images/images/instagram.svg'
+import facebook from '../public/images/images/facebook.svg'
+import linkedin from '../public/images/images/linkedin.svg'
+import Image from 'next/image'
+
 type Props = {}
+const socialMedia = [
+  {
+    id: 'social-media-1',
+    icon: instagram,
+    link: 'https://www.instagram.com/',
+  },
+  {
+    id: 'social-media-2',
+    icon: facebook,
+    link: 'https://www.facebook.com/',
+  },
+  {
+    id: 'social-media-3',
+    icon: twitter,
+    link: 'https://www.twitter.com/',
+  },
+  {
+    id: 'social-media-4',
+    icon: linkedin,
+    link: 'https://www.linkedin.com/',
+  },
+]
+
+
 function Footer({}: Props) {
   return (
     <div className="fixed  bottom-0 justify-between bg-[#1c1c28]  text-sm font-light px-10 flex flex-row w-full border-gray-600 border-t-[1px] h-10 items-center z-10">
@@ -11,9 +41,21 @@ function Footer({}: Props) {
           <WifiIcon className="h-6 w-6 " />
         </div>
         <div className="hover:opacity-70 cursor-pointer">Support</div>
-        <div className="hover:opacity-70 cursor-pointer">Contact us</div>
         <div className="hover:opacity-70 cursor-pointer">Feedback</div>
+        <div className="hover:opacity-70 cursor-pointer">Contact us</div>
+        <div className="flex flex-row space-x-3">
+          {socialMedia.map((social, index) => (
+            <Image
+              key={social.id}
+              src={social.icon}
+              alt={social.id}
+              className="cursor-pointer"
+              onClick={() => window.open(social.link)}
+            />
+          ))}
+        </div>
       </div>
+
       <div className="flex flex-row space-x-10 items-center">
         <div className="flex flex-row items-center space-x-2">
           <ChartBarIcon className="h-4 w-4 text-green-600" />
